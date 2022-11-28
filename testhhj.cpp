283af8f6d5f3d73e9d89a1fb5693623bf4fcb583
NoIgnore <1209172438@qq.com>
2022-11-28 12:23:58 +0800

    测试文件

---
+++ testhhj.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s = "10023";
+    cout << stoi(s);
+    int abc = 100;
+    cout << to_string(abc);
+    return 0;
+}
